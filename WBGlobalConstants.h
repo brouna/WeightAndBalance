6ae9bd13f8ba4d0f964d4fce8c016e894f870c54
Adam Broun <abroun@gmail.com>
2014-07-28 12:49:00 -0400

    3.1.4  Hopefully fixed corrupt archive file

--- WBGlobalConstants.h
+++ WBGlobalConstants.h
@@ -17,7 +17,7 @@
 #define TKS_LBS_PER_GALLON  @9.1
 
 
-#define TYPES_LIBRARY_RESOURCE_NAME     @"initialtypes.v2.archive"      //the name of the templates in the package
+#define TYPES_LIBRARY_RESOURCE_NAME     @"initialtypes.v3.archive"      //the name of the templates in the package
 #define TYPES_LIBRARY_FILE_NAME     @"types.v2.archive"             // the name of the types in the app file structure after deployment
 #define AIRCRAFT_LIBRARY_FILE_NAME   @"aircraft.archive"
 
